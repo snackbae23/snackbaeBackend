efb7197eab0888d04308026bad26777d73fab256
Tanmay <121565463+tanmay2809@users.noreply.github.com>
2024-02-24 05:08:59 +0530

    Update menu.js

--- Controllers/menu.js
+++ Controllers/menu.js
@@ -1,20 +1,34 @@
 const AddMenu = require("../models/AddMenu");
-
+const restaurantLogin = require("../models/restaurantLogin")
 // Function to add a menu item
 const addMenuItem = async (req, res) => {
-  const { ItemName, cuisineType, Type, Cuisines } = req.body;
-  const imageUrl = req.file.location; // Assuming the URL of the uploaded image from DigitalOcean Spaces
+  const restaurantId = req.params.restaurantId;
+  const { menuItem, type, pic, cuisines } = req.body;
 
   try {
-    const menuItem = new AddMenu({
-      ItemName,
-      cuisineType,
-      image: imageUrl,
-      Type,
-      Cuisines,
+    // Create a new menu item
+    const newMenuItem = new AddMenu({
+      menuItem,
+      type,
+      pic,
+      cuisines,
     });
 
-    const newMenuItem = await menuItem.save();
+    // Save the new menu item
+    await newMenuItem.save();
+
+    // Find the restaurant by ID
+    const restaurant = await restaurantLogin.findOneAndUpdate(
+      { _id: restaurantId },
+      { $push: { menu: newMenuItem._id } },
+      { new: true } // Return the updated document
+    );
+
+    // Check if the restaurant exists
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
     res.status(201).json(newMenuItem);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -22,13 +36,33 @@ const addMenuItem = async (req, res) => {
 };
 
 const getMenuItems = async (req, res) => {
+  const restaurantId = req.params.restaurantId;
+
   try {
-    const menuItems = await AddMenu.find();
-    res.json(menuItems);
+    // Find the restaurant by ID
+    const restaurant = await restaurantLogin.findOne({ _id: restaurantId });
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
+    // Array to store all menu item IDs
+    let menuItemIds = [];
+
+    // Iterate over each menu in the restaurant
+    for (const menu of restaurant.menu) {
+      // Concatenate menu item IDs to menuItemIds array
+      menuItemIds = menuItemIds.concat(menu._id);
+    }
+
+    // Find all menu items for the restaurant using the concatenated menuItemIds array
+    const menuItems = await AddMenu.find({ _id: { $in: menuItemIds } });
+
+    res.status(200).json(menuItems);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(400).json({ message: err.message });
   }
 };
+
 
 module.exports = {
   addMenuItem,
